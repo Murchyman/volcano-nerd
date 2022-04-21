a1935fe5223d64ffc07e191e341c90e19a10a943
johndoe@example.com <johndoe@example.com>
2022-04-21 11:29:07 +1000

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import styles from '../styles/Navbar.module.css'
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <div className={styles.NavbarLogo}>
+                    🌋 VolcanoNerd.com
+                </div>
+                <div className={styles.NavBarLinks}>
+
+                    <div className={styles.linkItem}><NavLink to="/" >Home</NavLink></div>
+                    <div className={styles.linkItem}><NavLink to="/search" >Search</NavLink></div>
+                    <div className={styles.linkItem}><NavLink to="/login" >Login</NavLink></div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
