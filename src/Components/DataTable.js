9869ad6326be8c9d51a0cf89ab5ee3f39158f0f6
John Doe <johndoe@example.com>
2022-04-22 09:08:55 +1000

    link working

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
 
 
 
@@ -7,24 +8,28 @@ export default function DataTable(props) {
     const columns = [
         { field: 'title', headerName: 'Title', width: 300 },
         { field: 'author_name', headerName: 'Author', width: 200 },
-        { field: 'button', headerName: 'Button', width: 100 },
-        //render link
         {
-            field: 'button',
-            headerName: 'Button',
-            width: 100,
-            render: (rowData) => {
-                return (
-                    <a href={`/book/${rowData.key}`}>
-                        <button>View</button>
-                    </a>
-                );
-            }
-        }
-
+            field: 'link',
+            headerName: 'Learn More',
+            width: 150,
+            renderCell: (params) => (
+              <strong>
+                <Button
+                href={'https://www.google.com/search?q=' + params?.value}
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  style={{ marginLeft: 16 }}
+                >
+                  Open
+                </Button>
+              </strong>
+            ),
+          },
+        
     ];
 
-    const rows = props.Data.map((item, i) => ({ id: i, title: item?.title, author_name: item?.authors[0]?.name, }));
+    const rows = props.Data.map((item, i) => ({ id: i, title: item?.title, author_name: item?.authors[0]?.name, link: item?.title }));
 
     return (
         <div style={{ height: 400, width: '100%' }}>
