b06e08871fb768bc50e56b5db75b9872ad6539d6
johndoe@example.com <johndoe@example.com>
2022-04-22 11:43:56 +1000

    country dropdown+population dropdown working

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -7,42 +7,48 @@ import Button from '@mui/material/Button';
 export default function DataTable(props) {
 
 
-    
-    const columns = [
-        { field: 'title', headerName: 'Title', width: 300 },
-        { field: 'author_name', headerName: 'Author', width: 200 },
-        {
-            field: 'link',
-            headerName: 'Learn More',
-            width: 130,
-            renderCell: (params) => (
-              <strong>
-                <Button
-                href={'https://www.google.com/search?q=' + params?.value}
-                  variant="contained"
-                  color="primary"
-                  size="small"
-                  style={{ marginLeft: 16 }}
-                >
-                  Open
-                </Button>
-              </strong>
-            ),
-          },
-        
-    ];
 
-    const rows = props.Data.map((item, i) => ({ id: i, title: item?.title, author_name: item?.authors[0]?.name, link: item?.title }));
+  const columns = [
+    { field: 'id', headerName: 'ID', flex: 0.2 },
+    { field: 'name', headerName: 'Name', flex: 1 },
+    { field: 'country', headerName: 'Country', flex: 0.5 },
+    { field: 'region', headerName: 'Elevation', flex: 1 },
+    { field: 'subregion', headerName: 'Subregion', flex: 1 },
+    {
+      field: 'link',
+      headerName: 'Learn More',
+      flex: 0.5,
+      renderCell: (params) => (
 
-    return (
-        <div style={{ height: '60%', width: '40em' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[10]}
-                loading={props.Loading}
-            />
-        </div>
-    );
+        <Button
+          href={'https://www.google.com/search?q=' + params?.value}
+          variant="contained"
+          color="primary"
+          size="small"
+          style={{ marginLeft: 16 }}
+        >
+          Open
+        </Button>
+
+      ),
+    },
+
+  ];
+
+  const rows = props.Data.map((item, i) => ({ id: item?.id, name: item?.name, country: item?.country, region: item?.region, subregion: item.subregion, link: item?.name }));
+
+  return (
+    <div style={{ width: '100%' }} >
+      <DataGrid
+        autoHeight={true}
+        disableExtendRowFullWidth={true}
+        autoPageSize={true}
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+        loading={props.Loading}
+      />
+    </div>
+  );
 }
--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,33 +1,64 @@
-import DataTable from '../Components/DataTable';
-import { useEffect, useState } from 'react';
-import styles from '../styles/Search.module.css';
+import DataTable from "../Components/DataTable";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import countries from "../data/countries.json";
+import { useState } from "react";
+import styles from "../styles/Search.module.css";
 function Search() {
+    const [data, setData] = useState([]);
+    const [isloding, setIsloding] = useState(false);
+    const [searchCountry, setSearchCountry] = useState("");
+    const [searchPopulatedDistance, setSearchPopulatedDistance] = useState("");
+    const GetData = (endpoint) => {
+        setIsloding(true);
+        fetch(`http://sefdb02.qut.edu.au:3001/volcanoes?country=${searchCountry}&populatedWithin=${searchPopulatedDistance}`)
+            .then((response) => response.json())
+            .then((json) => {
+                setData(json);
+            })
+            .then(() => {
+                setIsloding(false);
+            });
+    };
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.menu}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={countries}
+                        onInputChange={(event, newInputValue) => {
+                            setSearchCountry(newInputValue);
+                        }}
 
-  useEffect(() => {
-    GetData();
-  }, []);
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Countries" />}
+                    />{" "}
+                    <Autocomplete
+                        disablePortal
+                        onInputChange={(event, newInputValue) => {
+                            setSearchPopulatedDistance(newInputValue);
+                        }}
+                        id="combo-box-demo"
+                        options={distances}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Populated Within" />}
+                    />{" "}
+                    <button onClick={GetData}>Search</button>
+                </div>
+                <h1>The Books</h1>
 
-  const [data, setData] = useState([]);
-const [isloding, setIsloding] = useState(true);
-  const GetData = () => {
-    fetch('http://openlibrary.org/subjects/cooking.json')
-      .then(response => response.json())
-      .then(json => {
-        setData(json.works);
-      })
-      .then(() => {
-        setIsloding(false);
-      }
-      );
-  };
-  return (
-    <div className={styles.wrapper}>
-        <div className={styles.container}>
-        <h1>The Books</h1>
-        <DataTable Data={data} Loading={isloding} />
+                <DataTable Data={data} Loading={isloding} />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
+
+const distances = [
+    { label: "5km" },
+    { label: "10km" },
+    { label: "30km" },
+    { label: "100km" },]
 
 export default Search;
